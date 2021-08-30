1d62fa3b29118ed3c8bfe642afb75954697dcae6
Sousa <lucioadelson@gmail.com>
2021-08-29 20:52:02 -0400

    OK

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
+const readline = require('readline-sync')
 
 function start() {
     const content = {}
 
     content.searchTerm = askAndReturnSearchTerm()
+    content.prefix = askAndReturnPrefix()
 
     function askAndReturnSearchTerm() {
         return question('Type a Wikipedia search term: ')
+    }
+
+    function askAndReturnPrefix() {
+        const prefixes = ['Who is', 'What is', 'The History Of']
+        const selectedPrefixIndex = readline.keyInSelect(prefixe, 'Choose one option: ')
+
+        console.log(selectedPrefixIndex)
     }
 
     console.log(content)
